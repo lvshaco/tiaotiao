29e4da3db4fa0f2349e484fdb8465d6480b22da4
lvshaco <lvshaco@gmail.com>
2016-05-21 11:24:16 +0800

    add gameover rank

---
+++ src/packet/GameOver.js
@@ -0,0 +1,75 @@
+const assert = require('assert');
+function GameOver(my, clients) {
+    var cl = clients.slice(0);
+    var idx = cl.indexOf(my);
+    if (idx !=-1) {
+        cl.splice(idx,1);
+    }
+    cl.push(my);
+    this.clients = cl;
+}
+
+module.exports = GameOver;
+/*
+struct GameOver {
+    struct {
+        uint32_t rank;
+        uint32_t roleid;
+        uint32_t sex;
+        uint32_t province;
+        uint32_t city;
+        uint32_t mass;
+        uint32_t eat;
+        uint32_t copper;
+        uint32_t exp;
+        uint8_t name_len; // 名字的长度
+        name bytes;// 名字字节
+    } vector;
+} */
+GameOver.prototype.build = function() {
+    var count = this.clients.length;
+    for (var i=0; i<count; ++i) {
+        var c = this.clients[i]
+        len = len + 36+c.getName().length + 1;
+    }
+    var buflen = 1 + len;
+
+    var buf = new ArrayBuffer(buflen);
+    var view = new DataView(buf);
+
+    view.setUint8(0, 18, true); // Packet ID
+    var offset = 1;
+    for (var i=0; i<count; ++i) {
+        view.setUint32(offset, c.rank, true); offset += 4;
+        view.setUint32(offset, c.info.roleid, true); offset += 4;
+        view.setUint32(offset, c.info.sex, true); offset += 4;
+        view.setUint32(offset, c.info.province, true); offset += 4;
+        view.setUint32(offset, c.info.city, true); offset += 4;
+        view.setUint32(offset, c.mass, true); offset += 4;
+        view.setUint32(offset, c.eat, true); offset += 4;
+        view.setUint32(offset, c.copper, true); offset += 4;
+        view.setUint32(offset, c.exp, true); offset += 4;
+
+        var c = this.clients[i];
+        var name = c.getName();
+        if (name) {
+            view.setUint8(offset, name.length, true);
+            offset += 1;
+            for (var j = 0; j < name.length; j++) {
+                var c = name.charCodeAt(j);
+                if (c) {
+                    view.setUint8(offset, c, true);
+                }
+                offset += 1;
+            }
+        } else {
+            view.setUint8(offset, 0, true);
+            offset += 1;
+        }
+    }
+    if (offset != buflen) {
+    console.log("GameOver offset="+offset+" buflen="+buflen);
+    }
+    console.log("GameOver size="+buflen);
+    return buf;
+};
---
+++ src/packet/UpdateRank.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+function UpdateRank(clients, count) {
+    this.clients = clients;
+    this.count = count;
+}
+
+module.exports = UpdateRank;
+
+UpdateRank.prototype.build = function() {
+    // roleid, name_len, name
+    
+    var count = this.clients.length;
+    if (count > this.count) {
+        count = this.count;
+    }
+    var len = 0;
+    for (var i=0; i<count; ++i) {
+        var c = this.clients[i]
+        len = len + 4+c.getName().length + 1;
+    }
+    var buflen = 1 + len;
+
+    var buf = new ArrayBuffer(buflen);
+    var view = new DataView(buf);
+
+    view.setUint8(0, 17, true); // Packet ID
+    var offset = 1;
+    for (var i=0; i<count; ++i) {
+        view.setUint32(offset, c.info.roleid, true);
+        offset += 4;
+        var c = this.clients[i];
+        var name = c.getName();
+        if (name) {
+            view.setUint8(offset, name.length, true);
+            offset += 1;
+            for (var j = 0; j < name.length; j++) {
+                var c = name.charCodeAt(j);
+                if (c) {
+                    view.setUint8(offset, c, true);
+                }
+                offset += 1;
+            }
+        } else {
+            view.setUint8(offset, 0, true);
+            offset += 1;
+        }
+    }
+    if (offset != buflen) {
+    console.log("UpdateRank offset="+offset+" buflen="+buflen);
+    }
+    console.log("UpdateRank size="+buflen);
+    return buf;
+};
